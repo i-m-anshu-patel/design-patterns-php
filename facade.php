db10a5346473bf016b2816e5352e4ddd28b51e65
anshu_patel_001 <anshu@easyecom.io>
2023-05-05 12:06:51 +0530

    Facede Design Pattern

---
+++ facade.php
@@ -0,0 +1,50 @@
+<?php
+interface Shape{
+    public function draw();
+}
+
+class Rectangle implements Shape{
+    public function draw(){
+        echo "Inside Rectangle \n";
+    }
+}
+
+class Square implements Shape{
+    public function draw(){
+        echo "Inside Square \n";
+    }
+}
+
+
+class Circle implements Shape{
+    public function draw(){
+        echo "Inside Circle \n";
+    }
+}
+
+
+class ShapeMaker{
+    private $circle;
+    private $rectangle;
+
+    private $square;
+
+    public function __construct(){
+        $this->circle = new Circle();
+        $this->rectangle = new Rectangle();
+        $this->square = new Square();
+    }
+
+    public function drawCircle(){
+       return $this->circle->draw();
+    }
+    public function drawRectangle(){
+       return $this->rectangle->draw();
+    }
+
+    public function drawSquare(){
+       return $this->square->draw();
+    }
+
+}
+?>
